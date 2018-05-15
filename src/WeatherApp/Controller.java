0603553b1ae12722453e8a3ec073fb29eb30d2cf
Geza Csenger <csengergeza@gmail.com>
2018-05-15 19:28:52 +0100

    very basic dashboard

--- src/WeatherApp/Controller.java
+++ src/WeatherApp/Controller.java
@@ -1,4 +1,27 @@
 package WeatherApp;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.SortedList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+
 public class Controller {
+    @FXML
+    private Button editButton;
+    @FXML
+    private Button settingsButton;
+    @FXML
+    private ListView<String> dashboardList;
+    private ObservableList<String> listContent = FXCollections.observableArrayList();
+
+    @FXML
+    private void editClicked(){
+        System.out.println("edit clicked");
+
+        listContent.add("edit clicked");
+        dashboardList.setItems(listContent);
+    }
 }
--- src/WeatherApp/Main.java
+++ src/WeatherApp/Main.java
@@ -1,18 +1,26 @@
 package WeatherApp;
 
 import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import jdk.jfr.EventType;
+
+import java.awt.event.ActionEvent;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("../scenes/dashboard.fxml"));
+
+        Scene scene = new Scene(root, 300, 275);
+
+        primaryStage.setTitle("Tempest");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
