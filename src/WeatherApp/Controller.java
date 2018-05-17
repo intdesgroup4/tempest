a1684bcd1657528c497275ffd60614ecdeedca95
shrimpleypimples <34160186+shrimpleypimples@users.noreply.github.com>
2018-05-17 12:56:44 +0100

    basic dashboard
    added the fieldcapsule templates so on click of "add field" we get the field capsules added in a scrollplane:
    currently commented out the map starting on press of add

--- src/WeatherApp/Controller.java
+++ src/WeatherApp/Controller.java
@@ -4,43 +4,43 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 
 public class Controller {
-    @FXML
-    private Button editButton;
-    @FXML
-    private Button settingsButton;
-    @FXML
-    private Button addButton;
-    @FXML
-    private ListView<String> dashboardList;
-    private ObservableList<String> listContent = FXCollections.observableArrayList();
-
-
+    @FXML private Button editButton;
+    @FXML private Button settingsButton;
+    @FXML private Button addButton;
+    @FXML private VBox dbcontent = new VBox();
+    @FXML private ScrollPane dashboardList = new ScrollPane(dbcontent);
 
     @FXML
     private void editClicked(){
         System.out.println("edit clicked");
-
-        listContent.add("edit clicked");
-        dashboardList.setItems(listContent);
     }
 
     @FXML
     private void addClicked() throws IOException{
-
+    	System.out.println("add clicked");
+    	Node fieldcap = FXMLLoader.load(getClass().getResource("../scenes/FieldCapsuleTemplate.fxml"));
+    	dbcontent.getChildren().add(fieldcap);
+    	dashboardList.setContent(dbcontent);
+    	/*
         Stage stage = (Stage)dashboardList.getScene().getWindow();
         Parent root = FXMLLoader.load(getClass().getResource("../scenes/mapScreen.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+        */
+        
     }
 }
--- src/WeatherApp/Main.java
+++ src/WeatherApp/Main.java
@@ -8,8 +8,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
-import jdk.jfr.EventType;
-
 import java.awt.event.ActionEvent;
 
 public class Main extends Application {
